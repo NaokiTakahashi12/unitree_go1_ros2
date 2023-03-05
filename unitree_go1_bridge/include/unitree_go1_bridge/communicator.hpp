0009bcf9163f00ce7b8589da203bd31c6e2e7781
NaokiTakahashi <naoki.takahashi060@gmail.com>
2023-03-05 22:48:46 +0900

    Add unitree communicator class

---
+++ unitree_go1_bridge/include/unitree_go1_bridge/communicator.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#define BOOST_BIND_NO_PLACEHOLDERS
+#include <unitree_legged_sdk/unitree_legged_sdk.h>
+
+
+namespace unitree_go1_bridge
+{
+namespace unitree_legged_sdk = UNITREE_LEGGED_SDK;
+
+class Communicator
+{
+public:
+  using Command = unitree_legged_sdk::LowCmd;
+  using State = unitree_legged_sdk::LowState;
+
+  Communicator();
+  ~Communicator();
+
+  void send(Command);
+
+  void receive();
+  void receive(State &);
+
+private:
+  uint8_t m_control_level;
+  uint16_t m_local_port;
+  uint16_t m_target_port;
+
+  std::string m_target_ip_address;
+
+  std::unique_ptr<unitree_legged_sdk::UDP> m_unitree_udp;
+  std::unique_ptr<unitree_legged_sdk::Safety> m_unitree_safety;
+
+  void ignoreScreenOut();
+  void enableScreenOut();
+};
+}  // namespace unitree_go1_bridge
---
+++ unitree_go1_bridge/src/communicator.cpp
@@ -0,0 +1,85 @@
+#include <unitree_go1_bridge/communicator.hpp>
+
+#include <iostream>
+
+
+namespace unitree_go1_bridge
+{
+Communicator::Communicator()
+: m_control_level(unitree_legged_sdk::LOWLEVEL),
+  m_local_port(static_cast<uint16_t>(8090)),
+  m_target_port(static_cast<uint16_t>(unitree_legged_sdk::UDP_SERVER_PORT)),
+  m_target_ip_address(unitree_legged_sdk::UDP_SERVER_IP_BASIC)
+{
+  ignoreScreenOut();
+  m_unitree_udp = std::make_unique<unitree_legged_sdk::UDP>
+  (
+    m_control_level,
+    m_local_port,
+    m_target_ip_address.c_str(),
+    m_target_port
+  );
+  m_unitree_safety = std::make_unique<unitree_legged_sdk::Safety>
+  (
+    unitree_legged_sdk::LeggedType::Go1
+  );
+  enableScreenOut();
+}
+
+Communicator::~Communicator()
+{
+  ignoreScreenOut();
+  if(m_unitree_udp)
+  {
+    m_unitree_udp.reset();
+  }
+  if(m_unitree_safety)
+  {
+    m_unitree_safety.reset();
+  }
+  enableScreenOut();
+}
+
+void Communicator::send(Command command)
+{
+  State state;
+  m_unitree_udp->GetRecv(state);
+
+  if(not m_unitree_safety)
+  {
+    throw std::runtime_error("unitree safety is null");
+  }
+  const auto protect_result = m_unitree_safety->PowerProtect(
+    command,
+    state,
+    1
+  );
+  if(protect_result < 0)
+  {
+    throw std::runtime_error("Error of unitree safety");
+  }
+  m_unitree_udp->SetSend(command);
+}
+
+void Communicator::receive()
+{
+  m_unitree_udp->Recv();
+}
+
+//! @param [out] received_state
+void Communicator::receive(State &received_state)
+{
+  receive();
+  m_unitree_udp->GetRecv(received_state);
+}
+
+void Communicator::ignoreScreenOut()
+{
+  std::cout.setstate(std::ios_base::failbit);
+}
+
+void Communicator::enableScreenOut()
+{
+  std::cout.clear();
+}
+}  // namespace unitree_go1_bridge
